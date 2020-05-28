7121e15ed1798f19f28dc03e3928ce5c19f11155
Florian Bruggisser <florian@bildspur.ch>
2020-05-28 22:09:16 +0200

    added addressbook example

--- src/main/java/ch/bildspur/Main.java
+++ src/main/java/ch/bildspur/Main.java
@@ -1,10 +1,22 @@
 package ch.bildspur;
 
-import com.example.tutorial.AddressBookProtos;
+import com.example.tutorial.AddressBookProtos.Person;
 
 public class Main {
 
     public static void main(String[] args) {
-        
+        Person john =
+                Person.newBuilder()
+                        .setId(1234)
+                        .setName("John Doe")
+                        .setEmail("jdoe@example.com")
+                        .addPhones(
+                                Person.PhoneNumber.newBuilder()
+                                        .setNumber("555-4321")
+                                        .setType(Person.PhoneType.HOME))
+                        .build();
+
+        System.out.println(john.getEmail());
+        System.out.println(john.toByteString());
     }
 }
